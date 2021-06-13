37643edb8e66022ba94ef2de09f999bbd583f186
Kelli Landry <landry.kel@gmail.com>
2021-06-13 17:40:19 -0500

    Create model w/ classes, add type to Todo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import React from 'react'
 import './App.css'
-
+import Todos from '../src/components/Todos'
+import Todo from './models/todo'
 function App () {
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn TypeScript')
+  ]
   return (
     <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Todos items={todos} />
     </div>
   )
 }
